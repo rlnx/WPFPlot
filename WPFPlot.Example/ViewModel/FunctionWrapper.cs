21390bf3e1e84d5fad90812c47335b139d9df178
RuslanIsrafilov <ruslan.is@outlook.com>
2014-09-18 22:39:57 +0400

    WPFPlot usage example added.

---
+++ WPFPlot.Example/ViewModel/FunctionWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using WPFPlot.Data;
+
+namespace WPFPlot.Example.ViewModel
+{
+	public class FunctionWrapper : IEvaluateable
+	{
+		private Func<double, double> mFunc;
+
+		public FunctionWrapper(Func<double, double> func)
+		{
+			Contract.Requires(func != null);
+			mFunc = func;
+		}
+
+		public double Evaluate(double x)
+		{ return mFunc(x); }
+
+	}
+}
---
+++ WPFPlot.Example/ViewModel/MainModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFPlot.Data;
+
+namespace WPFPlot.Example.ViewModel
+{
+	public class MainModel
+	{
+
+		public MainModel()
+		{
+			var f1 = new FunctionWrapper(x => Math.Sin(x)     );
+			var f2 = new FunctionWrapper(x => 2 * Math.Cos(x));
+			var f3 = new FunctionWrapper(x => -Math.Sin(x));
+			var f4 = new FunctionWrapper(x => -2 * Math.Cos(x));
+
+			Test1 = new ContinuousPlotDataSource() { Function = f1 };
+			Test2 = new ContinuousPlotDataSource() { Function = f2 };
+			Test3 = new ContinuousPlotDataSource() { Function = f3 };
+			Test4 = new ContinuousPlotDataSource() { Function = f4 };
+		}
+
+		public IPlotDataSource Test1 { get; private set; }
+		public IPlotDataSource Test2 { get; private set; }
+		public IPlotDataSource Test3 { get; private set; }
+		public IPlotDataSource Test4 { get; private set; }
+
+	}
+}
